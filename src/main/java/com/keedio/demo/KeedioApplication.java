4781f7f700ccf8774f61cb8b4aad9fd7ee5b85b8
isanmartin <isanmartin@keedio.com>
2018-06-30 18:59:24 +0200

    Modified controller

--- src/main/java/com/keedio/demo/KeedioApplication.java
+++ src/main/java/com/keedio/demo/KeedioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KeedioApplication {
+public class KeedioApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(KeedioApplication.class, args);
