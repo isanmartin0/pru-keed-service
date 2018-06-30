e95a3d16b7040bb483a9144984d4feeda66a0179
isanmartin <isanmartin@keedio.com>
2018-06-30 18:36:20 +0200

    Modified controller

--- src/main/java/com/keedio/demo/controllers/DemoController.java
+++ src/main/java/com/keedio/demo/controllers/DemoController.java
@@ -4,14 +4,16 @@ package com.keedio.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/keedio")
 public class DemoController {
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/keedio")
+    @GetMapping(value = "/hello")
     public String hello() {
         return "Hello from Keedio";
     }
