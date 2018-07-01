095e8a7bbf3d73322a6d7c0fa782ceceb9ff64b0
isanmartin <isanmartin@keedio.com>
2018-07-01 19:25:57 +0200

    Modified dependencies

--- src/main/java/com/evobanco/digitalservices/subscribetags/service/TagsRepository.java
+++ src/main/java/com/evobanco/digitalservices/subscribetags/service/TagsRepository.java
@@ -1,9 +1,7 @@
 package com.evobanco.digitalservices.subscribetags.service;
 
 import com.evobanco.digitalservices.subscribetags.domain.Tag;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TagsRepository extends Repository<Tag, Long> {
-
-  Tag save(Tag persisted);
+public interface TagsRepository extends CrudRepository<Tag, Long> {
 }
