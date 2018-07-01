131e9d6dccd051f294309f9f076fefd1eea2ea79
isanmartin <isanmartin@keedio.com>
2018-07-01 14:29:36 +0200

    Added get mapping

--- src/main/java/com/evobanco/digitalservices/subscribetags/SubscribeTags.java
+++ src/main/java/com/evobanco/digitalservices/subscribetags/SubscribeTags.java
@@ -2,9 +2,10 @@ package com.evobanco.digitalservices.subscribetags;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SubscribeTags {
+public class SubscribeTags extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscribeTags.class, args);
